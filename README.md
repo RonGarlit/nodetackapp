5e42602855367700b99e699961e86d1eb470b4fb
Ronald Garlit <rongarlit@live.com>
2018-06-10 19:35:02 -0400

    Updated routes, added comments, JQuery and Popper Dependencies

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 "# nodetackapp" 
+
+Based on the tutorial code Jay at CodeHandBook.org
 https://codehandbook.org/web-app-using-node-js-and-mysql/
--- app.js
+++ app.js
@@ -7,11 +7,28 @@ app.listen(3000,function(){
 });
 
 
+// In the app.js file, define the static folder paths so that it gets resolved in the node application
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 app.use('/style',  express.static(__dirname + '/style'));
 
-
+// Now let’s define the default route for our application.
 app.get('/',function(req,res){
     res.sendFile('home.html',{'root': __dirname + '/templates'});
+})
+
+/* 
+As seen in the above code, when the / route is requested the home.html page is rendered. Save the above 
+changes and restart the server. 
+
+Point browser URL to http://localhost:3000 and you should be able to view the home page.
+*/
+
+// Next define the routes to show the sign in page and the sign up page in app.js
+app.get('/showSignInPage',function(req,res){
+    res.sendFile('signin.html',{'root': __dirname + '/templates'});
+})
+
+app.get('/showSignUpPage',function(req,res){
+  res.sendFile('signup.html',{'root':__dirname + '/templates'})
 })
